84c80b355bb223c975ff650d6af8f4bf08d90555
d4n-m <dumy.tm@gmail.com>
2021-10-13 14:08:19 +0200

    final

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -3,7 +3,7 @@ import users from '../data/users.js';
 class User {
 
     createUser(user) {
-        if(!users.find(element => (element.username == user.username)))
+        if (!users.find(element => (element.username == user.username)))
             users.push(user);
         else return "Usuario ya existe"
         return this.getUser(user);
@@ -13,17 +13,20 @@ class User {
         return users.find(element => (element.username == user.username))
     }
 
-    getUserRoutes(){}
+    getUserRoutes() { }
 
-    getUsers(){
+    getUsers() {
         return users;
     }
 
-    deleteUser(username){
-        console.log(username)
-        users.splice(users.findIndex((v) =>{
+    deleteUser(username) {
+        const result = users.findIndex((v) => {
             return v.username === username
-          }), 1);
+        })
+        
+        if (result >= 0) {
+            users.splice(result, 1);
+        }
         return "Usuario borrado";
     }
 }
