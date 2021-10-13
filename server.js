ec9635a510c6f604f256fe5669517f869e97092c
d4n-m <dumy.tm@gmail.com>
2021-10-13 11:30:12 +0200

    Prepare proj for Heroku deploy

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 import app from './app.js';
 
-app.listen(process.env.PORT || 3000, () => console.log(`listen on port ${process.env.PORT}`));
+let port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`listen on port ${port}`));
